ca160b2bd92ec8c9ec9c5098fd5c1a6a4485ac34
Lauren <technical.me@aol.com>
2018-08-04 23:24:10 -0400

    fine

--- keys.js
+++ keys.js
@@ -1,15 +1,12 @@
 
 console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: `B0T3lipcyxz8GjAQrgn8AKEZt`,
-  consumer_secret: `B0OBSFFAWJOSlaYUxrUS6wMPtjU0yxKKbrrTPB9b7ER5dNrs8g`,
-  access_token_key: `10196554848302899219Mgf5VIF0TTcCSF6ZLi93Xip1ytBJQ`,
-  access_token_secret: `RtuxMFCw5c0rMFunQpmu4W4ZKvTGp8lpsJEKEhYoJGD0W`
-
+exports.spotify = {
+  id: process.env4ff4dea3f74b41eeafd989534685e202,
+  secret: process.envd8acda1bb78e4036bea66a7c97484626 
 };
-
-// exports.spotify = {
-//   id: process.env.SPOTIFY_ID,
-//   secret: process.env.SPOTIFY_SECRET
-// };
+exports.twitter = {
+  consumer_key: B0T3lipcyxz8GjAQrgn8AKEZt,
+  consumer_secret: B0OBSFFAWJOSlaYUxrUS6wMPtjU0yxKKbrrTPB9b7ER5dNrs8g,
+  access_token_key: process.env10196554848302899219Mgf5VIF0TTcCSF6ZLi93Xip1ytBJQ,
+  access_token_secret: RtuxMFCw5c0rMFunQpmu4W4ZKvTGp8lpsJEKEhYoJGD0W
+};
